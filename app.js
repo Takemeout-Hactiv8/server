9c79bfaaaed78cb7e79cec878d34505f69a70d88
Vincent <vincentcan909@gmail.com>
2024-08-14 14:42:32 +0700

    Feat: change port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const { createServer } = require("http");
 const { Server } = require("socket.io");
 
 const app = express();
-const port = 3000;
+const port = 80;
 
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
